d1ebdd637680de4cbbe53a57179b14b264a04d72
Maik <maikman@users.noreply.github.com>
2020-09-18 09:09:13 +0200

    changed host

--- config/prod.exs
+++ config/prod.exs
@@ -29,7 +29,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :etsala, EtsalaWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "etsala.space", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   https: [
