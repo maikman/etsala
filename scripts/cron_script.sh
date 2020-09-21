b561ef64ae4b461ab6c2ebf358a763923af809b8
Maik <maikman@users.noreply.github.com>
2020-09-21 18:51:24 +0200

    changed mix location

--- scripts/cron_script.sh
+++ scripts/cron_script.sh
@@ -2,25 +2,25 @@
 
 function import_order_history {
   echo "$(timestamp) start import order history"
-  /lib/elixir/bin/mix run /root/etsala/scripts/order_history.exs
+  /usr/bin/mix run /root/etsala/scripts/order_history.exs
   echo "$(timestamp) import finished"
 }
 
 function import_orders {
   echo "$(timestamp) start import orders"
-  /lib/elixir/bin/mix run /root/etsala/scripts/orders.exs
+  /usr/bin/mix run /root/etsala/scripts/orders.exs
   echo "$(timestamp) import finished"
 }
 
 function import_types {
   echo "$(timestamp) start import types"
-  /lib/elixir/bin/mix run /root/etsala/scripts/types.exs
+  /usr/bin/mix run /root/etsala/scripts/types.exs
   echo "$(timestamp) import finished"
 }
 
 function import_systems {
   echo "$(timestamp) start import systems"
-  /lib/elixir/bin/mix run /root/etsala/scripts/systems.exs
+  /usr/bin/mix run /root/etsala/scripts/systems.exs
   echo "$(timestamp) import finished"
 }
 
