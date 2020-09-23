619e1e61f6b10ffc61b5251f1e4140b406cdb87e
Maik <maikman@users.noreply.github.com>
2020-09-23 15:37:00 +0200

    handle station/structure link

--- lib/etsala_web/objects/structure.ex
+++ lib/etsala_web/objects/structure.ex
@@ -1,21 +1,35 @@
 defmodule EtsalaWeb.Objects.Structure do
-    defstruct [
-        :name,
-        :owner_id,
-        :position,
-        :solar_system_id,
-        :type_id,
-        :id
-    ]
+  defstruct [
+    :name,
+    :owner_id,
+    :position,
+    :solar_system_id,
+    :type_id,
+    :id,
+    :type
+  ]
 
-    def new(esi_structure) do
-        %__MODULE__{
-            id: esi_structure["id"],
-            name: esi_structure["name"],
-            owner_id: esi_structure["owner_id"],
-            position: esi_structure["position"],
-            solar_system_id: esi_structure["solar_system_id"],
-            type_id: esi_structure["type_id"]
-        }
-    end
+  def new(esi_structure = %{"station_id" => _station_id}) do
+    %__MODULE__{
+      id: esi_structure["id"],
+      name: esi_structure["name"],
+      owner_id: esi_structure["owner_id"],
+      position: esi_structure["position"],
+      solar_system_id: esi_structure["solar_system_id"],
+      type_id: esi_structure["type_id"],
+      type: :station
+    }
+  end
+
+  def new(esi_structure) do
+    %__MODULE__{
+      id: esi_structure["id"],
+      name: esi_structure["name"],
+      owner_id: esi_structure["owner_id"],
+      position: esi_structure["position"],
+      solar_system_id: esi_structure["solar_system_id"],
+      type_id: esi_structure["type_id"],
+      type: :structure
+    }
+  end
 end
