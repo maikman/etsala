e94dad9fc416c6d4d7544e4072354873e4498970
Maik <maikman@users.noreply.github.com>
2020-10-10 23:26:24 +0200

    added deps to the script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,8 @@ echo "PULL REPO"
 git pull origin master
 echo "KILL SERVER"
 ps aux | grep elixir | grep -v grep | awk '{print $2}' | xargs kill
+echo "INSTALL DEPS"
+mix deps.get
 echo "EXECUTE MIGRATIONS"
 mix ecto.migrate
 echo "DEPLOY ASSETS"
