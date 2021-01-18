5125a986f50481fc5f0c31d8b0f6318bea4d5370
Maik <maikman@users.noreply.github.com>
2021-01-18 21:42:55 +0100

    async market data loading

--- lib/etsala_web/controllers/type_controller.ex
+++ lib/etsala_web/controllers/type_controller.ex
@@ -2,7 +2,7 @@ defmodule EtsalaWeb.TypeController do
   use EtsalaWeb, :controller
   alias EtsalaWeb.Router.Helpers, as: Routes
   alias Etsala.Eve.Universe.Types
-  alias Etsala.Eve.Market.Order
+  # alias Etsala.Eve.Market.Order
   alias WDI.ESI.Images
 
   def types(conn, _params) do
@@ -11,17 +11,17 @@ defmodule EtsalaWeb.TypeController do
   end
 
   def type_details(conn, %{"id" => name}) do
-    access_token = get_session(conn, :access_token)
+    # access_token = get_session(conn, :access_token)
 
     type =
       name
       |> Tools.Formatter.decode_name()
       |> Types.get_type_by_name()
 
-    market_orders =
-      Order.get_sell_order_by_type_id(type.type_id)
-      |> Enum.map(&EtsalaWeb.Objects.TypeOrder.new(&1, access_token))
-      |> Enum.sort_by(&{&1.name, &1.price})
+    # market_orders =
+    #   Order.get_sell_order_by_type_id(type.type_id)
+    #   |> Enum.map(&EtsalaWeb.Objects.TypeOrder.new(&1, access_token))
+    #   |> Enum.sort_by(&{&1.name, &1.price})
 
     details =
       %{}
@@ -30,7 +30,7 @@ defmodule EtsalaWeb.TypeController do
       |> Map.put(:description, type.description |> format_description(conn))
       |> Map.put(:image_url, Images.get_image(type.type_id, 64))
 
-    render(conn, "type_details.html", details: details, market_orders: market_orders)
+    render(conn, "type_details.html", details: details)
   end
 
   def format_description(nil, _), do: ""
--- lib/etsala_web/live/type_details_live.ex
+++ lib/etsala_web/live/type_details_live.ex
@@ -11,14 +11,34 @@ defmodule EtsalaWeb.TypeDetailsLive do
 
   @impl true
   def mount(_params, session, socket) do
+    send(self(), {:load_sell_data, session["access_token"], session["details"]})
+
+    access_token = session["access_token"]
+    type = session["details"]
+
     {:ok,
      assign(socket,
-       details: session["details"],
-       market_orders: session["market_orders"],
-       access_token: session["access_token"]
+       details: type,
+       market_orders: [],
+       access_token: access_token
      )}
   end
 
+  @impl true
+  def handle_info({:load_sell_data, access_token, type}, socket) do
+    # This thing fetches data from some external service or db, let's say
+    market_orders =
+      Order.get_sell_order_by_type_id(type.type_id)
+      |> Enum.map(&EtsalaWeb.Objects.TypeOrder.new(&1, access_token))
+      |> Enum.sort_by(&{&1.name, &1.price})
+
+    {:noreply,
+     assign(socket,
+       market_orders: market_orders
+     )}
+  end
+
+  @impl true
   def handle_event("market_details_sell", _params, socket) do
     market_orders =
       Order.get_sell_order_by_type_id(socket.assigns.details.type_id)
@@ -28,6 +48,7 @@ defmodule EtsalaWeb.TypeDetailsLive do
     {:noreply, assign(socket, market_orders: market_orders)}
   end
 
+  @impl true
   def handle_event("market_details_buy", _params, socket) do
     market_orders =
       Order.get_buy_order_by_type_id(socket.assigns.details.type_id)
@@ -36,22 +57,4 @@ defmodule EtsalaWeb.TypeDetailsLive do
 
     {:noreply, assign(socket, market_orders: market_orders)}
   end
-
-  # def handle_event("suggest", %{"q" => _query}, socket) do
-  #   {:noreply, assign(socket, matches: [])}
-  # end
-
-  # def handle_event("search", %{"q" => query}, socket) when byte_size(query) <= 100 do
-  #   send(self(), {:search, query})
-  #   {:noreply, assign(socket, query: query, result: "Searching...", loading: true, matches: [])}
-  # end
-
-  # def handle_info({:search, query}, socket) do
-  #   result =
-  #     Etsala.Eve.Universe.Types.search_type(query)
-  #     |> Enum.map(& &1.name)
-  #     |> Enum.sort_by(&byte_size/1)
-
-  #   {:noreply, assign(socket, loading: false, result: result, matches: [])}
-  # end
 end
